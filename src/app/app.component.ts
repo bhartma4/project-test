77a7b827a93cf6dc7a1782857538e396149e8d2e
bha1097 <hartmanbrette@gmail.com>
2025-01-05 15:35:51 -0500

    added info component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeInfoComponent } from "./home-info/home-info.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeInfoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/home-info/home-info.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-info',
+  imports: [],
+  templateUrl: './home-info.component.html',
+  styleUrl: './home-info.component.css'
+})
+export class HomeInfoComponent {
+
+}
